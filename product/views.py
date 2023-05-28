07843eb05efaeead38579577ad0691c84459eed2
Dikihmd01 <dikihamdani.id@gmail.com>
2023-05-28 20:20:35 +0700

    update: product detail page

--- product/views.py
+++ product/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def detail(request, pk):
-    product = get_list_or_404(Product, pk=pk)
-    context = {'product': product}
-    print(context)
+    product = get_object_or_404(Product, pk=pk)
+    related_products = Product.objects.filter(
+        category=product.category,
+        is_sold=False,
+    ).exclude(pk=pk)[:3]
+    context = {
+        'product': product,
+        'related_products': related_products
+    }
 
     return render(request, 'product/product_detail.html', context)
