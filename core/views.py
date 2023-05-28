20bb91c031d442638e83b88ce69444ec79a0ec98
Dikihmd01 <dikihamdani.id@gmail.com>
2023-05-28 15:44:51 +0700

    Add: home content - newest product

--- core/views.py
+++ core/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from product.models import Category, Product
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    products = Product.objects.filter(is_sold=False)[:6]
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'core/index.html', context)
+
 
 def about(request):
     return render(request, 'core/about.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
