4d7bbb0a6b0680a58670f63d3c6a6cfc741d0be6
Dikihmd01 <dikihamdani.id@gmail.com>
2023-05-28 20:43:55 +0700

    update: dynamic navigation items

--- core/views.py
+++ core/views.py
@@ -5,16 +5,22 @@ from product.models import Category, Product
 def index(request):
     products = Product.objects.filter(is_sold=False)[:6]
     categories = Category.objects.all()
+    active_item = request.path
     context = {
         'categories': categories,
-        'products': products
+        'products': products,
+        'active_item': active_item
     }
     return render(request, 'core/index.html', context)
 
 
 def about(request):
-    return render(request, 'core/about.html')
+    active_item = request.path
+    context = {'active_item': active_item}
+    return render(request, 'core/about.html', context)
 
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    active_item = request.path
+    context = {'active_item': active_item}
+    return render(request, 'core/contact.html', context)
